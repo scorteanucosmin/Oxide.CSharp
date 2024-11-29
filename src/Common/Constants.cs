a9c56e89dfa3cbef8a9a00f36631e3be71558b7b
scorteanucosmin <cozminsc@gmail.com>
2024-11-29 15:42:40 +0200

    Moved constants to Constants class file

--- src/Common/Constants.cs
+++ src/Common/Constants.cs
@@ -1,10 +1,20 @@
-﻿using Oxide.CSharp.CompilerStream;
+﻿using System.Text.RegularExpressions;
+using Oxide.CSharp.CompilerStream;
 using Oxide.CSharp.Interfaces;
 
 namespace Oxide.CSharp.Common
 {
     internal static class Constants
     {
+        //TODO: Move to Oxide.Common
         internal static readonly ISerializer Serializer = new Serializer();
+
+        internal const string CompilerDownloadUrl = "https://downloads.oxidemod.com/artifacts/Oxide.Compiler/{0}/";
+        internal const string CompilerBasicArguments = "-unsafe true --setting:Force true -ms true";
+
+        internal static readonly Regex FileErrorRegex = new Regex(@"^\[(?'Severity'\S+)\]\[(?'Code'\S+)\]\[(?'File'\S+)\] (?'Message'.+)$",
+            RegexOptions.Compiled);
+
+        internal static readonly Regex SymbolEscapeRegex = new Regex(@"[^\w\d]", RegexOptions.Compiled);
     }
 }
